10c9f4fe914234d64b70055eb82a8d2b36545694
arhsis <arhsis2024@gmail.com>
2025-04-02 14:16:03 +0800

    update
    Signed-off-by: arhsis <arhsis2024@gmail.com>

--- setup.sh
+++ setup.sh
@@ -5,20 +5,19 @@ touch /tmp/init.log
 
 exec > /tmp/init.log 2>&1
 
-# c220g5 specific
-disk_dev=/dev/sda4
-work_dir=/pythia
+work_dir=/work
 repo=https://github.com/arhsis/pythia.git
 
 function setup_workdir() {
     sudo mkdir -p $work_dir
     sudo /usr/local/etc/emulab/mkextrafs.pl $work_dir
+    sudo chmod a+rw $work_dir
 }
 
 
 function setup_champsim() {
-    git clone $repo $work_dir
-    cd $work_dir
+    git clone $repo $work_dir/pythia
+    cd $work_dir/pythia
     git checkout dev
 
     git clone https://github.com/mavam/libbf.git libbf
@@ -29,8 +28,8 @@ function setup_champsim() {
 }
 
 function download_traces() {
-    mkdir -p $work_dir/traces
-    wget -O $work_dir/traces/603.bwaves_s-1080B.champsimtrace.xz https://dpc3.compas.cs.stonybrook.edu/champsim-traces/speccpu/603.bwaves_s-1080B.champsimtrace.xz
+    mkdir -p $work_dir/pythia/races
+    wget -O $work_dir/pythia/traces/603.bwaves_s-1080B.champsimtrace.xz https://dpc3.compas.cs.stonybrook.edu/champsim-traces/speccpu/603.bwaves_s-1080B.champsimtrace.xz
 }
 
 function setup_git_info() {
@@ -47,16 +46,15 @@ EOF
 
 function setup_utils() {
    sudo apt-get update
-   sudo apt-get --yes install neovim tmux htop autojump ripgrep perl clang-format
+   sudo apt-get --yes install cmake neovim tmux htop autojump ripgrep perl clang-format
    # conda env, autojump, set default editor to nvim
-   sudo sh -c 'echo "export PATH=\"/tip/miniforge3/bin:\$PATH\"" >> /root/.bashrc && \
-   echo ". /usr/share/autojump/autojump.sh" >> /root/.bashrc && \
+   sudo sh -c ' echo ". /usr/share/autojump/autojump.sh" >> /root/.bashrc && \
    echo "export VISUAL=nvim" >> /root/.bashrc && \
    echo "export EDITOR=\"\$VISUAL\"" >> /root/.bashrc'
 }
 
+setup_utils
 setup_workdir
 setup_champsim
 download_traces
-setup_utils
 setup_git_info
